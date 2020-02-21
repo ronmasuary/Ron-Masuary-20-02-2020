e1757efd719a7750723448f355fb6271b8cf8153
Ron Masuary <ronmas2@gmail.com>
2020-02-21 02:11:41 +0200

    Updates

---
+++ precache-manifest.41d2615b22e723e2f49eb2087b82d4fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5721ec36cfef6ca1ecdc726c7cdf65ea",
+    "url": "/Ron-Masuary-20-02-2020/index.html"
+  },
+  {
+    "revision": "999466d2fdbf1f344ae9",
+    "url": "/Ron-Masuary-20-02-2020/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b299df2d97f36f6c8e34",
+    "url": "/Ron-Masuary-20-02-2020/static/css/main.4dc100a2.chunk.css"
+  },
+  {
+    "revision": "999466d2fdbf1f344ae9",
+    "url": "/Ron-Masuary-20-02-2020/static/js/2.23ca2809.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Ron-Masuary-20-02-2020/static/js/2.23ca2809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b299df2d97f36f6c8e34",
+    "url": "/Ron-Masuary-20-02-2020/static/js/main.e573c3f8.chunk.js"
+  },
+  {
+    "revision": "c442a8592ff2a131b275",
+    "url": "/Ron-Masuary-20-02-2020/static/js/runtime-main.f10f0e32.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0dc136376e3433589d57a9ce30a575c5.js"
+  "/Ron-Masuary-20-02-2020/precache-manifest.41d2615b22e723e2f49eb2087b82d4fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ron-Masuary-20-02-2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
